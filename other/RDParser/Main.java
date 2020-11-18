c4ac18fd0004eb7ff51fdc38987491a1f99eb09f
Erewhon-Lucy <1663523296@qq.com>
2020-11-18 09:55:33 +0800

    commit Sort compare and MergeSortThreaded

---
+++ other/RDParser/Main.java
@@ -0,0 +1,40 @@
+package other.RDParser;
+
+
+import java.io.File;
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.PrintWriter;
+
+
+public class Main {
+
+    //-----------------------------------------------------------------------------------------------------------
+    public static void main(String[] args) throws IOException {
+
+        if (args.length == 0) {                               //checking if a file was passed from the command line
+            System.out.println("No file was entered.");     //if not then exit program
+            System.out.println("Exiting the program.");
+            System.exit(0);
+        }
+
+        File testFile = new File(args[0]);                                     //checking to see if the file exists or not
+        if (testFile.exists() == false) {                                        //if not then exit the program
+            System.out.println("The file: " + args[0] + " does not exist.");
+            System.out.println("Exiting the program.");
+            System.exit(0);
+        }
+        Scan lexical = new Scan(args[0]);
+
+        lexical.getTokens();
+        RecursiveDescentParser paser = new RecursiveDescentParser();
+        paser.parse();
+
+
+        return;
+    } //end of main()
+//-----------------------------------------------------------------------------------------------------------
+} //end of Main Class
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////
